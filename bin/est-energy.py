16476c2129880adca269cb80a641a98b43d4d26f
Jonathan Dorn <dornja@gmail.com>
2016-03-03 11:25:28 -0500

    est-energy.py now reads coefficients from a file (in new etc dir by default)

--- bin/est-energy.py
+++ bin/est-energy.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 from contextlib import contextmanager
-from numpy.distutils import cpuinfo
+from collections import defaultdict
+import csv
 from optparse import OptionParser
 import os
+import platform
 from subprocess import call, check_call, Popen
 import sys
 from tempfile import NamedTemporaryFile
+
+root = os.path.dirname( os.path.dirname( os.path.abspath( sys.argv[ 0 ] ) ) )
 
 parser = OptionParser(
     usage = "%prog [options] -- command [args...]"
@@ -15,8 +21,9 @@ parser.add_option(
     "-o", metavar = "file", help = "write estimated Joules to the named file"
 )
 parser.add_option(
-    "--max-simultaneous-counters", metavar = "N", type = int, default = 5,
-    help = "maximum number of counters that can be collected simultaneously"
+    "--model", metavar = "csv",
+    default = os.path.join( root, "etc", "energy-models.csv" ),
+    help = "CSV file containing coefficients for the power model"
 )
 options, args = parser.parse_args()
 
@@ -32,68 +39,95 @@ def mktemp( suffix = '' ):
 
 def collect_counters( counters, cmd, stdout = sys.stdout, stderr = sys.stderr):
     result = dict()
-    n = options.max_simultaneous_counters
-    if len( counters ) > n:
-        with open( "/dev/null", 'w' ) as devnull:
-            for group in [ counters[i:i+n] for i in range( 0, len( counters ), n ) ]:
-                result.update( collect_counters( group, cmd, stdout, stderr ) )
-                stdout = devnull
-                stderr = devnull
-    else:
-        with mktemp() as datfile:
-            cmd = [
-                "perf", "stat", "-o", datfile, "-e", ",".join( counters ), "--"
-            ] + cmd
-            try:
-                check_call( cmd, stdout = stdout, stderr = stderr )
-            except KeyboardInterrupt:
-                exit( 127 )
-            counters += [ "seconds" ]
-            with open( datfile ) as fh:
-                for line in fh:
-                    line = line.split()
-                    if len( line ) >= 2 and \
-                            line[ 0 ][ 0 ].isdigit() and \
-                            line[ 1 ] in counters:
-                        result[ line[ 1 ] ] = float( line[ 0 ].replace( ",", "" ) )
+
+    # Always run at least once, then stop when there are no more counters left
+
+    remaining = list( counters )
+    with open( "/dev/null", 'w' ) as devnull:
+        while True:
+            with mktemp() as datfile:
+                perf = [ "perf", "stat", "-o", datfile ]
+                if len( remaining ) > 0:
+                    perf += [ "-e", ",".join( remaining ) ]
+                perf += [ "--" ]
+                try:
+                    check_call( perf + cmd, stdout = stdout, stderr = stderr )
+                except KeyboardInterrupt:
+                    exit( 127 )
+
+                gotone = False
+                with open( datfile ) as fh:
+                    for line in fh:
+                        line = line.split()
+                        if len( line ) < 2 or \
+                                not line[ 0 ][ 0 ].isdigit() or \
+                                line[ 1 ] not in [ "seconds" ] + remaining:
+                            continue
+                        gotone = True
+                        while line[ 1 ] in remaining:
+                            remaining.remove( line[ 1 ] )
+                        result[ line[1] ] = float( line[0].replace( ",", "" ) )
+            if not gotone or len( remaining ) == 0:
+                break
+            stdout = devnull
+            stderr = devnull
     return result
 
-def intel_sandybridge_power_model( d ):
+def power_model( coeff, d ):
     return d[ "seconds" ] * (
-          31.530 +
-          20.490 * ( d[ "instructions" ] / d[ "cycles" ] ) +
-           9.838 * ( ( d[ "r532010" ] + d[ "r538010" ] ) / d[ "cycles" ] ) +
-          -4.102 * ( d[ "cache-references" ] / d[ "cycles" ] ) +
-        2962.678 * ( d[ "cache-misses" ] / d[ "cycles" ] )
-    )
+        float( coeff[ "cycles" ] ) * d[ "cycles" ] +
+        float( coeff[ "instrs" ] ) * d[ "instructions" ] +
+        float( coeff[ "floats" ] ) * get_float_counters( d ) +
+        float( coeff[ "cache" ] )  * d[ "cache-references" ] +
+        float( coeff[ "misses" ] ) * d[ "cache-misses" ]
+    ) / d[ "cycles" ]
 
-#This is the trained energy coefficients for Prime
-def intel_xeon_prime_power_model( d ):
-    return d[ "seconds" ] * (
-          151.396 +
-          14.575 * ( d[ "instructions" ] / d[ "cycles" ] ) +
-          2.681 * ( ( d[ "r532010" ] + d[ "r538010" ] ) / d[ "cycles" ] ) +
-          -806.480 * ( d[ "cache-references" ] / d[ "cycles" ] ) +
-          597.546 * ( d[ "cache-misses" ] / d[ "cycles" ] )
-    )
-   
+########
+# Select the power model
+########
 
-counters = [
-    "cycles",
-    "instructions",
-    "cache-references",
-    "cache-misses",
-]
-if cpuinfo.cpuinfo().is_AMD():
-    counters += [ "r533f00" ]
+with open( "/proc/cpuinfo" ) as fh:
+    for line in fh:
+        if line.startswith( "vendor_id" ):
+            cpu_vendor = line.split()[ 2 ]
+            break
+if cpu_vendor == "AuthenticAMD":
+    def get_float_counters( d ):
+        return d[ "r533f00" ]
+elif cpu_vendor == "GenuineIntel":
+    def get_float_counters( d ):
+        return d[ "r532010" ] + d[ "r538010" ]
 else:
-    counters += [ "r532010", "r538010" ]
+    print( "ERROR: unexpected CPU vendor:", cpu_vendor, file = sys.stderr )
+    exit( 2 )
+
+coefficients = dict()
+with open( options.model ) as fh:
+    reader = csv.DictReader( fh )
+    for row in reader:
+        if row[ "host" ] == platform.node():
+            coefficients = row
+            break
+    else:
+        print( "ERROR: could not find hostname ({0}) in {1}".format(
+            platform.node(), options.model
+        ), file = sys.stderr )
+        exit( 2 )
+
+########
+# Use the power model we selected
+########
+
+# determine the counters needed by the model...
+
+required = defaultdict( lambda: 1.0 )
+power_model( coefficients, required )
+counters = [ counter for counter in required.keys() if counter != "seconds" ]
 
 if options.o is None:
     fh = sys.stdout
 else:
     fh = open( options.o, 'w' )
-print(intel_sandybridge_power_model( collect_counters( counters, args ) ), file=fh)
-#Prime power model
-#print(intel_xeon_prime_power_model( collect_counters( counters, args ) ), file=fh)
+data = collect_counters( counters, args )
+print( power_model( coefficients, data ), file = fh )
 
