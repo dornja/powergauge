e363b3d348a4bda03764c9b25f511004b8edb79c
Jeremy Lacomis <lacomis@virginia.edu>
2016-07-12 17:16:04 -0400

    Fix compile to work when called anywhere

--- benchmarks/libav/compile.sh
+++ benchmarks/libav/compile.sh
@@ -2,27 +2,26 @@
 
 LIBAV="libav-src"
 SRC="src"
-CURDIR=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $# -ne 0 ] ; then
     echo "$0 takes no arguments. Assumes that get_sources.sh has set up the $LIBAV and $SRC directories"
     exit
 fi
 
-if [ ! -d "$LIBAV" ] ; then
+if [ ! -d "$DIR/$LIBAV" ] ; then
     echo "$LIBAV folder not found. Did you run get_sources.sh?"
     exit 1
 fi
 
-if [ ! -d "$SRC" ] ; then
+if [ ! -d "$DIR/$SRC" ] ; then
     echo "$SRC folder not found. Did you run get_sources.sh?"
     exit 1
 fi
 
 echo "Copying files"
-rsync -ar "$SRC/" "$LIBAV"
+rsync -ar "$DIR/$SRC/" "$DIR/$LIBAV"
 echo "Compiling"
 cd "$LIBAV"
 pwd
-make -f "$CURDIR/Makefile" avconv avplay avprobe
-cd "$CURDIR" > /dev/null
+make -f "$DIR/Makefile" avconv avplay avprobe
