d149b2c634522aa0c386b5b437c53c0055ff60b3
Jeremy Lacomis <lacomis@virginia.edu>
2016-08-02 18:01:01 -0400

    Add mpeg4 test, fix compile.sh completely

--- benchmarks/libav/compile.sh
+++ benchmarks/libav/compile.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
 LIBAV="libav-src"
-SRC="src"
+SRC=`dirname "$1"`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ $# -ne 0 ] ; then
-    echo "$0 takes no arguments. Assumes that get_sources.sh has set up the $LIBAV and $SRC directories"
-    exit
-fi
+# if [ $# -ne 0 ] ; then
+#     echo "$0 takes no arguments. Assumes that get_sources.sh has set up the $LIBAV and $SRC directories"
+#     exit
+# fi
 
 if [ ! -d "$DIR/$LIBAV" ] ; then
     echo "$LIBAV folder not found. Did you run get_sources.sh?"
     exit 1
 fi
 
-if [ ! -d "$DIR/$SRC" ] ; then
-    echo "$SRC folder not found. Did you run get_sources.sh?"
-    exit 1
-fi
+# if [ ! -d "$DIR/$SRC" ] ; then
+#     echo "$SRC folder not found. Did you run get_sources.sh?"
+#     exit 1
+# fi
 
-echo "Copying files"
-rsync -ar "$DIR/$SRC/" "$DIR/$LIBAV"
-echo "Compiling"
+rsync -arc "$SRC/" "$DIR/$LIBAV"
 cd "$LIBAV"
-pwd
-make -f "$DIR/Makefile" avconv avplay avprobe
+make -f "$DIR/Makefile" avconv &> /dev/null
