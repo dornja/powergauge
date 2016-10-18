a40e76c12d8798a0becca2251509fb570a862f54
Jonathan Dorn <dornja@gmail.com>
2016-10-18 10:49:24 -0400

    fixed parse-genprog-log flags to take uncertainty and repeated fitness evaluations into account

--- bin/parse-genprog-log.py
+++ bin/parse-genprog-log.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+from collections import namedtuple
 import csv
+import heapq
 from optparse import OptionParser
 import re
 import sys
@@ -14,14 +16,22 @@ parser.add_option(
     help = "only include a subset of variants"
 )
 parser.add_option(
+    "--final", action = "store_true",
+    help = "always report the last fitness evaluation for each variant"
+)
+parser.add_option(
     "--stop-after", metavar = "N", type = int,
     help = "stop after reading the first N variants"
 )
 parser.add_option(
-    "--sort", action="store_true", dest="sort", help = "sort by amount of improvement in fitness. You likely want to include --filter=steps as well"
+    "--sort", action="store_true", dest="sort",
+    help = "sort by amount of improvement in fitness. Implies --filter=steps " \
+        + " and --final."
 )
 parser.add_option(
-    "--no-variant-names", action="store_true", dest="no_variant_names", help="do not output variant names. Greatly reduces file size and R plot times"
+    "--no-variant-names", action="store_true", dest="no_variant_names",
+    help="do not output variant names. Greatly reduces file size and R plot " \
+        + "times"
 )
 options, args = parser.parse_args()
 
@@ -29,12 +39,12 @@ if len( args ) < 1:
     parser.print_help()
     exit()
 
-def optmax( a, b ):
-    if a is None:
-        return b
-    if b is None:
-        return a
-    return max( a, b )
+if options.sort:
+    options.filter = "steps"
+    options.final = True
+
+Entry = namedtuple( "Entry", "gen fitness interval variant" )
+DiffEntry = namedtuple( "DiffEntry", "delta evalno fitness interval variant" )
 
 def getEntries( linesrc ):
     # A pattern for floating point numbers. This appears a couple times in the
@@ -55,8 +65,9 @@ def getEntries( linesrc ):
         m = variant_pat.search( line )
         if m is not None:
             fitness = float( m.group( 1 ) )
+            interval = 0.0 if m.group( 2 ) is None else float( m.group( 2 ) )
             variant = m.group( 3 )
-            yield gen, fitness, variant
+            yield Entry( gen, fitness, interval, variant )
             count += 1
             if options.stop_after is not None and count >= options.stop_after:
                 break
@@ -66,44 +77,75 @@ def getEntries( linesrc ):
             gen = int( m.group( 1 ) )
             continue
 
+numEntries = 0
+original   = None
+best       = list()
+unique     = dict()
 def statsFilter( entries ):
-    global best
     global numEntries
-    global unique
     global original
 
-    for gen, fitness, variant in entries:
+    for entry in entries:
         numEntries += 1
-        unique.add( variant )
-        if variant == "original":
-            original = fitness
-        best = optmax( best, fitness )
-        yield gen, fitness, variant
+        if entry.variant == "original":
+            original = entry
+
+        # Heap key uses negative fitness because heapq provides a minheap. Thus
+        # the most negative key will be first, which corresponds to the largest
+        # fitness value
+
+        heap_key = [ - entry.fitness - entry.interval, numEntries, entry ]
+        if entry.variant in unique:
+            # "remove" the previous entry from the heap
+            unique[ entry.variant ][ -1 ] = None
+        heapq.heappush( best, heap_key )
+        unique[ entry.variant ] = heap_key
+
+        yield entry
+
+def getBest():
+    while len( best ) > 0 and best[ 0 ][ -1 ] is None:
+        heapq.heappop( best )
+    if len( best ) > 0:
+        return best[ 0 ][ -1 ]
+    return None
 
 def regressionFilter( entries ):
-    global original
-
     pending = list()
     for entry in entries:
         pending.append( entry )
-        if original is None:
+        if original.fitness is None:
             continue
         while len( pending ) > 0:
-            gen, fitness, variant = pending.pop( 0 )
-            if fitness < original:
-                yield gen, fitness, variant
+            entry = pending.pop( 0 )
+            if entry.fitness < original.fitness or entry.variant == "original":
+                yield entry
 
 def stepsFilter( entries ):
-    current = None
-    for gen, fitness, variant in entries:
-        if current is None or current < fitness:
-            current = fitness
-            yield gen, fitness, variant
+    prev = None
+    for entry in entries:
+        curr = getBest()
+        if prev is not curr:
+            yield curr
+            prev = curr
 
-original = None
-best = None
-numEntries = 0
-unique = set()
+def retroFitnessFilter( entries ):
+    order = list()
+    for entry in entries:
+        order.append( entry.variant )
+    for variant in order:
+        yield unique[ variant ][ -1 ]
+
+def sortFilter( entries ):
+    cache = list()
+    prev = 0
+    for i, entry in enumerate( entries ):
+        diff = entry.fitness - prev
+        if diff > 0:
+            prev = entry.fitness
+        cache.append( ( diff, i, entry ) )
+    for diff, i, entry in sorted( cache ):
+        yield DiffEntry( diff, i, entry.fitness, entry.interval, entry.variant )
 
 if options.csv is None:
     out = open( "/dev/null", 'w' )
@@ -112,36 +154,29 @@ else:
 try:
     writer = csv.writer( out )
     if options.sort:
-        writer.writerow( [ "improvement", "evaluation", "fitness", "variant" ] )
-    elif options.no_variant_names:
-        writer.writerow( [ "generation", "fitness" ] )
+        header = [ "improvement", "eval number", "fitness", "interval" ]
     else:
-        writer.writerow( [ "generation", "fitness", "variant" ] )
+        header = [ "generation", "fitness", "interval" ]
+    if not options.no_variant_names:
+        header.append( "variant" )
+    writer.writerow( header )
+
     with open( args[ 0 ] ) as fh:
         source = statsFilter( getEntries( fh ) )
-        if options.filter is not None:
-            if options.filter == "steps":
-                source = stepsFilter( source )
-            elif options.filter == "regression":
-                source = regressionFilter( source )
+        if options.filter == "steps":
+            source = stepsFilter( source )
+        if options.final:
+            source = retroFitnessFilter( source )
+        if options.filter == "regression":
+            source = regressionFilter( source )
         if options.sort:
-            rows = list(source)
-            prev_best = 0
-            source = []
-            i = 0
-            for row in rows:
-                diff = row[1] - prev_best
-                if row[1] > prev_best:
-                    prev_best = row[1]
-                source.append((diff, i, row[1], row[2]))
-                i += 1
-            source.sort(key=lambda x: x[0])
-        for row in source:
+            source = sortFilter( source )
+        for entry in source:
             try:
                 if options.no_variant_names:
-                    writer.writerow( map( str, (row[0], row[1]) ) )
+                    writer.writerow( map( str, entry[ :-1 ] ) )
                 else:
-                    writer.writerow( map( str, row ) )
+                    writer.writerow( map( str, entry ) )
             except IOError as e:
                 # if this is piped to head, python will complain when the pipe
                 # is closed
@@ -153,15 +188,15 @@ finally:
 
 if options.csv is None:
     if original is not None:
-        print "original fitness:   ", original
-    print "best fitness:       ", best
+        print "original fitness:   ", original.fitness
+    print "best fitness:       ", getBest().fitness
     if original is not None:
         # Convert from fitness to energy
-        orig_energy = ( 1 / original ) - 1
-        best_energy = ( 1 / best ) - 1
+        orig_energy = ( 1 / original.fitness ) - 1
+        best_energy = ( 1 / getBest().fitness ) - 1
         print "original energy:    ", orig_energy
         print "best energy:        ", best_energy
-        print "fitness improvement: %2.4g%%" % ( ( ( best - original ) / original ) * 100 )
+        print "fitness improvement: %2.4g%%" % ( ( ( getBest().fitness - original.fitness ) / original.fitness ) * 100 )
         print "energy improvement:  %2.4g%%" % ( ( 1 - ( best_energy / orig_energy ) ) * 100 )
     print "variants considered:", numEntries
     print "unique variants:    ", len( unique )
