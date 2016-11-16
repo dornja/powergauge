f9fae9d00b523f763ab2fd228e9b3f8bd223ba25
Jeremy Lacomis <lacomis@virginia.edu>
2016-11-16 12:08:31 -0500

    Change libav tests

--- benchmarks/libav/test.py
+++ benchmarks/libav/test.py
@@ -7,8 +7,7 @@ root = os.path.abspath( sys.argv[ 0 ] )
 for i in range( 3 ):
     root = os.path.dirname( root )
 sys.path.append( os.path.join( root, "lib" ) )
-os.chdir("libav-src")
-from testutil import ParallelTest
+from testutil import Multitmp, ParallelTest
 from optparse import OptionGroup, OptionParser
 from distutils.spawn import find_executable
 from util import infomsg
@@ -16,77 +15,107 @@ from util import infomsg
 # Run test.py -h to get usage information
 
 class LibavTest( ParallelTest ):
-    def getGolden( self ):
-        return "outputs/%s%s" % ( self.test, self.getOutputSuffix() )
+    def getParser( self ):
+        return OptionParser(
+            usage = "%prog [options] __EXE_NAME__ test __FITNESS_FILE__"
+        )
+
+    def getOutputSuffix( self ):
+        return { "mpeg4":  ".avi",
+                 "prores": ".mov",
+        }[ self.test ]
 
     def getCommand( self, outfile ):
         cmd = [ self.exe ]
         cmd += {
-            "jpegls":   ['fate-vsynth1-jpegls', 
-                         '',
-                         '',
-                         root + '/benchmarks/libav/libav-src',
-                         """enc_dec "rawvideo -s 352x288 -pix_fmt yuv420p" tests/data/vsynth1.yuv avi "-c jpegls -sws_flags neighbor+full_chroma_int" rawvideo "-s 352x288 -pix_fmt yuv420p -sws_flags area" -keep""",
-                         '',
-                         'genprog-tests/ref/vsynth/vsynth1-jpegls',
-                         '',
-                         '1',
-                         '',
-                         '',
-                         '',
-                         '',
-                         '',
-                         '1',
-                         '',
-                         '',
-                         outfile],
-            "mpeg4":    ['fate-vsynth1-mpeg4', 
-                         '',
-                         '',
-                         root + '/benchmarks/libav/libav-src',
-                         """enc_dec "rawvideo -s 352x288 -pix_fmt yuv420p" tests/data/vsynth1.yuv mp4 "-c mpeg4 -qscale 10 -flags +mv4 -mbd bits" rawvideo "-s 352x288 -pix_fmt yuv420p " -keep""",
-                         '',
-                         'genprog-tests/ref/vsynth/vsynth1-mpeg4',
-                         '',
-                         '1',
-                         '',
-                         '',
-                         '',
-                         '',
-                         '',
-                         '1',
-                         '',
-                         '',
-                         outfile],
+            "mpeg4":    [ "-i", "inputs/eledream_640x360_8.y4m",
+                          "-codec", "mpeg4",
+                          "-vtag", "xvid",
+                          "-qscale", "2",       # 1-31
+                          "-an",
+                          outfile],
+            "prores":  [ "-i", "inputs/eledream_640x360_8.y4m",
+                         "-c:v", "prores",
+                         "-quant_mat", "proxy", # {proxy, lt, standard, hq}
+                         "-profile:v", "proxy", # {proxy, lt, standard, hq}
+                         "-mbs_per_slice", "8", # 1-8
+                         "-qscale", "0",        # 0-64
+                         "-an",
+                         outfile]
         }[ self.test ]
-        return cmd, dict() #{ "stderr": outfile }
+        return cmd, dict()
 
-    def getParser( self ):
-        return OptionParser(
-            usage = "%prog [options] test __FITNESS_FILE__"
-        )
+    def getGolden( self ):
+        if self.options.error:
+            return "outputs/%s" % self.test
+        else:
+            return "outputs/%s%s" % ( self.test, self.getOutputSuffix() )
+
+    def validateCorrectness( self, outfile ):
+        if self.options.error:
+            with Multitmp( len( outfile ) ) as outdir:
+                Multitmp.check_call( [ "rm", "-rf", outdir ], verbose = self.options.verbose )
+                Multitmp.check_call( [ "mkdir", outdir ], verbose = self.options.verbose )
+                Multitmp.check_call(
+                    [ "avconv",
+                      "-i", outfile,
+                      "-r", "25",
+                      "-loglevel", "panic",
+                      "%03d.png"
+                    ],
+                    verbose = self.options.verbose,
+                    cwd = outdir
+                )
+                if not ParallelTest.validateCorrectness( self, outdir ):
+                    return False
+                with Multitmp( len( outfile ) ) as result:
+                    with open( "/dev/null", 'w' ) as null:
+                        golden = self.getGolden()
+                        diffimg = os.path.join( root, "benchmarks",
+                                                "x264", "diff-img.sh")
+                        Multitmp.check_call(
+                            [ diffimg,
+                              golden,
+                              outdir
+                            ],
+                            stdout = result, stderr = null,
+                            verbose = self.options.verbose,
+                        )
+                    errors = list()
+                    for fname in result:
+                        with open( fname ) as fh:
+                            error = 0
+                            for line in fh:
+                                if line.startswith( "total" ):
+                                    error += float( line.split()[ 2 ] )
+                            errors.append( 1 / ( error + 1 ) )
+                    self.error = errors
+                    return True
+        else:
+            return ParallelTest.validateCorrectness( self, outfile )
+
+
+    def diff( self, golden, actual):
+        return True
+        # if self.options.error:
+        #     return True
+        # else:
+        #     return ParallelTest.diff( self, golden, actual )
+
+
+    def getParallelFitness( self, root, metrics ):
+        results = ParallelTest.getParallelFitness( self, root, metrics )
+        if self.options.error:
+            if results == [ [ 0 ] ]:
+                return [ [ 0 ], [ 0 ] ]
+            results.append( self.error )
+            return results
+        else:
+            return results
 
     def checkArgs( self, parser, args ):
-        if len( args ) < 2:
-            parser.print_help()
-            raise ValueError( "insufficient arguments" )
-        self.exe  = "tests/genprog-fate-run.sh"
-        use_default = True
-        for opt in [ self.options.emon, self.options.time, self.options.wu ]:
-            if opt is not None and opt is not False:
-                use_default = False
-        if len( self.options.rapl ) > 0:
-            use_default = False
-        if use_default:
-            self.options.model = True
-        self.test  = args[ 0 ]
-        # Horrible hack. Since we've chdir'd into libav-src the fitness file
-        # won't end up where expected, so "../" gets prepended unelss the fitness
-        # file path starts with a "/" (e.g., /dev/tty)
-        if args[ 1 ][ 0 ] == "/":
-            self.fitnessfile = args[ 1 ]
-        else:
-            self.fitnessfile = "../" + args[ 1 ]
+        ParallelTest.checkArgs( self, parser, args )
+        self.test = args[ 1 ]
 
 LibavTest().run( root )
 
