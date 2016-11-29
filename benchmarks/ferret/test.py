1beff9486dbc1588a76207a799c4b86e6004e1a4
Jeremy Lacomis <lacomis@virginia.edu>
2016-11-29 17:06:52 -0500

    Add error metric for ferret

--- benchmarks/ferret/test.py
+++ benchmarks/ferret/test.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from scipy import stats
 
 root = os.path.abspath( sys.argv[ 0 ] )
 for i in range( 3 ):
@@ -29,6 +30,78 @@ class FerretTest( ParallelTest ):
         }[ self.size ]
         cmd += [ outfile ]
         return cmd, dict()
+
+    def validateCorrectness( self, outfile ):
+        correctness = ParallelTest.validateCorrectness( self, outfile )
+        if self.options.error:
+            golden = self.getGolden()
+            gold_queries = dict()
+            with open( golden ) as fh:
+                for line in fh:
+                    gold_rank = dict()
+                    query = line.split()[0]
+                    ranks = line.split()[1:]
+                    for rank in ranks:
+                        image, rank = rank.split(":")
+                        rank = float(rank)
+                        gold_rank[image] = rank
+                    gold_queries[query] = gold_rank
+            errors = list()
+            for fname in outfile:
+                test_queries = dict()
+                # If the file isn't created, assign zero fitness
+                if not os.path.isfile(fname):
+                    return False
+                with open( fname ) as fh:
+                    for line in fh:
+                        test_rank = dict()
+                        query = line.split()[0]
+                        ranks = line.split()[1:]
+                        for rank in ranks:
+                            image, rank = rank.split(":")
+                            rank = float(rank)
+                            if image not in gold_queries[query]:
+                                gold_queries[query][image] = float("inf")
+                            test_rank[image] = rank
+                        test_queries[query] = test_rank
+                total_correlation = 0
+                for query in gold_queries:
+                    gold_rank = gold_queries[query]
+                    # If all the queries aren't performed, assign zero fitness
+                    try:
+                        test_rank = test_queries[query]
+                    except KeyError:
+                        return False
+                    gold_list = list()
+                    test_list = list()
+                    for image, rank in gold_rank.items():
+                        gold_list.append(rank)
+                        test_list.append(test_rank.get(image, float("inf")))
+                    tau, _ = stats.kendalltau(gold_list, test_list)
+                    total_correlation += tau
+                # Tau is a number between -1 and 1, this is a fix to not break our tools
+                error = 1 - total_correlation/len( gold_queries )
+                errors.append( 1 / ( error + 1 ) )
+            self.error = errors
+            return True
+        else:
+            return correctness
+
+    def diff( self, golden, actual):
+        if self.options.error:
+            return True
+        else:
+            return ParallelTest.diff( self, golden, actual )
+
+    def getParallelFitness( self, root, metrics ):
+        results = ParallelTest.getParallelFitness( self, root, metrics )
+        if self.options.error:
+            if results == [ [ 0 ] ]:
+                return [ [ 0 ], [ 0 ] ]
+            results.append( self.error )
+            return results
+        else:
+            return results
 
 FerretTest().run( root )
 
