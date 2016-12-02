baf7e6729a02eacece201e217731d870d6f02ff2
Jonathan Dorn <dornja@gmail.com>
2016-12-01 21:09:31 -0500

    moved genprog log parsing from parse-genprog-log tool to genprogutil library

--- bin/parse-genprog-log.py
+++ bin/parse-genprog-log.py
@@ -3,11 +3,17 @@
 from collections import namedtuple
 import csv
 import heapq
-from math import floor, isinf
+from math import floor
+import numpy as np
 from optparse import OptionParser
+import os
 import random
 import re
 import sys
+
+root = os.path.dirname( os.path.dirname( os.path.abspath( sys.argv[ 0 ] ) ) )
+sys.path.append( os.path.join( root, "lib" ) )
+from genprogutil import LogParser, Entry, Interval
 
 parser = OptionParser( usage = "%prog [options] logfile" )
 parser.add_option(
@@ -35,6 +41,10 @@ parser.add_option(
     help = "sort by amount of improvement in fitness column N"
 )
 parser.add_option(
+    "--no-confidence", action = "store_true",
+    help = "ignore confidence intervals when computing pareto frontiers"
+)
+parser.add_option(
     "--no-variant-names", action="store_true", dest="no_variant_names",
     help="do not output variant names. Greatly reduces file size and R plot " \
         + "times"
@@ -50,72 +60,16 @@ if options.filter == "best":
 if options.improvement:
     options.final = True
 
-Entry = namedtuple( "Entry", "gen evalno fitness variant" )
-
-class Interval:
-    def __init__( self, mean, delta ):
-        self.mean  = mean
-        self.delta = delta
-
-    def __neg__( self ):
-        return Interval( - self.mean, self.delta )
-
-    def __float__( self ):
-        return self.mean
-
-    def __lt__( self, other ):
-        if isinstance( other, Interval ):
-            return self.mean + self.delta < other.mean - other.delta
-        else:
-            return self.mean + self.delta < other
-
-    def __gt__( self, other ):
-        if isinstance( other, Interval ):
-            return self.mean - self.delta > other.mean + other.delta
-        else:
-            return self.mean - self.delta > other
-
-    def __eq__( self, other ):
-        if not isinstance( other, Interval ):
-            raise NotImplemented
-        return self.mean == other.mean and self.delta == other.delta
-
-    def __add__( self, other ):
-        if isinstance( other, Interval ):
-            return Interval( self.mean + other.mean, self.delta + other.delta )
-        else:
-            return Interval( self.mean + other, self.delta )
-
-    def __sub__( self, other ):
-        if isinstance( other, Interval ):
-            return Interval( self.mean - other.mean, self.delta + other.delta )
-        else:
-            return Interval( self.mean - other, self.delta )
-
-    def __rdiv__( self, other ):
-        if isinstance( other, Interval ):
-            return other.mean / self.mean
-        else:
-            return other / self.mean
-
-    def __str__( self ):
-        return "%s +/- %s" % ( str( self.mean ), str( self.delta ) )
-
-    def __repr__( self ):
-        return "Interval(%s,%s)" % ( repr( self.mean ), repr( self.delta ) )
-
-    def __hash__( self ):
-        return hash( ( self.mean, self.delta ) )
-
 class ParetoSpace1D:
-    def __init__( self ):
+    def __init__( self, lt ):
         self.best  = list()
         self.bykey = dict()
         self.count = 0
+        self.lt    = lt
 
-    @staticmethod
-    def dominates( a, b ):
-        return a > b
+    def dominates( self, a, b ):
+        # fitness values are 1-tuples containing Intervals
+        return self.lt( b[ 0 ], a[ 0 ] )
 
     def add( self, key, value ):
         if key in self.bykey:
@@ -148,17 +102,17 @@ class ParetoSpace1D:
 class ParetoSpaceND:
     Point = namedtuple( "Point", "key coords parent children" )
 
-    def __init__( self ):
+    def __init__( self, lt ):
         self.root = ParetoSpaceND.Point( None, None, [ None ], list() )
         self.points = dict()
+        self.lt = lt
 
-    @staticmethod
-    def dominates( a, b ):
+    def dominates( self, a, b ):
         better = False
         for x, y in zip( a, b ):
-            if x < y:
+            if self.lt( x, y ):
                 return False
-            elif not better and x > y:
+            elif not better and self.lt( y, x ):
                 better = True
         return better
 
@@ -169,8 +123,10 @@ class ParetoSpaceND:
             collected = list()
             for i, q in enumerate( parent.children ):
                 if not is_dominant and self.dominates( q.coords, p.coords ):
-                    random.shuffle( parent.children )
-                    worklist.append( ( q, 0, p ) )
+                    p.parent[ 0 ] = q
+                    q.children.append( p )
+                    #random.shuffle( parent.children )
+                    #worklist.append( ( q, 0, p ) )
                     break
                 elif self.dominates( p.coords, q.coords ):
                     collected.append( i )
@@ -211,10 +167,14 @@ class ParetoSpace:
 
     def add( self, key, value ):
         if self.delegate is None:
-            if len( value ) == 1:
-                self.delegate = ParetoSpace1D()
+            if options.no_confidence:
+                lt = lambda a, b: float( a ) < float( b )
             else:
-                self.delegate = ParetoSpaceND()
+                lt = lambda a, b: a < b
+            if len( value ) == 1:
+                self.delegate = ParetoSpace1D( lt )
+            else:
+                self.delegate = ParetoSpaceND( lt )
         self.delegate.add( key, value )
 
     def remove( self, key ):
@@ -225,103 +185,6 @@ class ParetoSpace:
         if self.delegate is not None:
             return self.delegate.getFrontier()
         return list()
-
-class parser:
-    def __init__( self, stream ):
-        self.CONFIG  = 0x1
-        self.DEBUG   = 0x2
-        self.FITNESS = 0x4
-
-        self.stream = self._getLine( stream )
-        self.fitness = list()
-        self.config = list()
-        self.debug = list()
-
-        next( self.stream )
-
-    def _getLine( self, stream ):
-        go_until = self.CONFIG | self.DEBUG | self.FITNESS
-
-        # A pattern for floating point numbers. This appears a couple times in
-        # the variant pattern, so I made a separate string to interpolate in
-        # instead of copying it verbatim and making a single extra-long pattern.
-        # Note that the pattern here does not capture any groups: the (?:) glyph
-        # indicates a non-capturing group in python.
-
-        f = r"-?(?:(?:\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)|inf)"
-        variant_pat = re.compile(
-            r"^\t\s*((?:{0}\s+(?:\+/-\s+{0}\s+)?)+)(.*)".format( f )
-        )
-        generation_pat = re.compile( r"generation (\d+) " )
-
-        gen = 0
-        count = 0
-        for line in stream:
-            m = variant_pat.search( line )
-            if m is not None:
-                if options.stop_after is not None and options.stop_after <= count:
-                    continue
-                count += 1
-                variant = m.group( 2 )
-                fitnesses, intervals = list(), list()
-                prev = None
-                for term in m.group( 1 ).split():
-                    if len( fitnesses ) == len( intervals ):
-                        fitnesses.append( float( term ) )
-                    elif prev == "+/-":
-                        intervals.append( float( term ) )
-                    elif term != "+/-":
-                        intervals.append( 0 )
-                        fitnesses.append( float( term ) )
-                    prev = term
-                if len( intervals ) < len( fitnesses ):
-                    intervals.append( 0 )
-                fitness = tuple( [
-                    Interval( m, d ) for m, d in zip( fitnesses, intervals )
-                ] )
-                self.fitness.append( Entry( gen, count, fitness, variant ) )
-                if go_until & self.FITNESS != 0:
-                    go_until = yield
-                continue
-            m = generation_pat.search( line )
-            if m is not None:
-                gen = int( m.group( 1 ) )
-                continue
-            if line.startswith( "--" ):
-                self.config.append( line.split( None, 1 ) )
-                if go_until & self.CONFIG != 0:
-                    go_until = yield
-                continue
-            self.debug.append( line )
-            if go_until & self.DEBUG != 0:
-                go_until = yield
-
-    def _consumer( self, queue, key ):
-        flipped = list()
-        while True:
-            while len( flipped ) > 0:
-                yield flipped.pop()
-            flipped = list( reversed( queue ) )
-            del queue[ : ]
-            if len( flipped ) == 0:
-                try:
-                    self.stream.send( key )
-                except StopIteration:
-                    break
-
-    def getConfig( self ):
-        for entry in self._consumer( self.config, self.CONFIG ):
-            yield entry
-
-    def getDebug( self ):
-        for entry in self._consumer( self.debug, self.DEBUG ):
-            yield entry
-
-    def getEntries( self ):
-        count = 0
-        for entry in self._consumer( self.fitness, self.FITNESS ):
-            yield entry
-            count += 1
 
 # Since the search may have been multi-objective, we keep track of the best
 # variants in a Pareto space.
@@ -340,13 +203,17 @@ def statsFilter( entries ):
         numEntries += 1
         if entry.variant == "original":
             original = entry
-        if all( [ x.mean == 0 or isinf( x.mean ) for x in entry.fitness ] ):
+        means = [ x.mean for x in entry.fitness ]
+
+        pessimal = np.all( means == 0 ) or np.any( np.isinf( means ) )
+        invalid  = np.any( np.isnan( means ) )
+        if pessimal or invalid:
             zeros += 1
 
         if unique.get( entry.variant, entry ).fitness != entry.fitness:
             best.remove( entry.variant )
             del unique[ entry.variant ]
-        if entry.variant not in unique:
+        if entry.variant not in unique and not invalid:
             best.add( entry.variant, entry.fitness )
             unique[ entry.variant ] = entry
 
@@ -358,11 +225,12 @@ def getBest():
 def bestFilter( entries ):
     for entry in entries:
         pass
-    nbest = list()
+    nbest = dict()
+    all_removed = list()
     while True:
         results = [ unique[ key ] for key, _ in best.getFrontier() ]
         done = False
-        for e1 in nbest:
+        for e1 in nbest.values():
             for e2 in results:
                 if best.dominates( e1.fitness, e2.fitness ):
                     done = True
@@ -372,10 +240,18 @@ def bestFilter( entries ):
         if done:
             break
         for e in results:
-            nbest.append( e )
             best.remove( e.variant )
+            all_removed.append( e )
+            if e.fitness in nbest:
+                l1 = len( nbest[ e.fitness ].variant.split() )
+                l2 = len( e.variant.split() )
+                if l1 <= l2:
+                    continue
+            nbest[ e.fitness ] = e
 
-    for entry in sorted( nbest, key = lambda e: ( e.gen, e.evalno ) ):
+    for entry in all_removed:
+        best.add( entry.variant, entry.fitness )
+    for entry in sorted( nbest.values(), key = lambda e: ( e.gen, e.evalno ) ):
         yield entry
 
 def regressionFilter( entries ):
@@ -413,27 +289,35 @@ def sortFilter( entries ):
         yield entry
 
 def improvementFilter( entries ):
+    # force "original" to be the first entry, so other improvements make sense
+    pending = list()
     prev = None
     for entry in entries:
+        pending.append( entry )
         if prev is None:
+            if entry.variant != "original":
+                continue
+            pending.pop()
+            pending.insert( 0, entry )
             diff = list( entry.fitness )
             prev = entry.fitness
-        else:
+        for entry in pending:
             diff = [ e - p for e, p in zip( entry.fitness, prev ) ]
             if any( [ float( d ) > 0 for d in diff ] ):
                 prev = entry.fitness
-        yield Entry(
-            entry.gen,
-            entry.evalno,
-            diff + list( entry.fitness ),
-            entry.variant
-        )
+            yield Entry(
+                entry.gen,
+                entry.evalno,
+                diff + list( entry.fitness ),
+                entry.variant
+            )
+        del pending[:]
 
 def show_fitness( fitness, original ):
     terms = list()
     for f_term, o_term in zip( fitness, original ):
-        f_term = f_term.mean if isinstance( f_term, Interval ) else f_term
-        o_term = o_term.mean if isinstance( o_term, Interval ) else o_term
+        f_term = float( f_term )
+        o_term = float( o_term )
         terms.append( "%g" % f_term )
         if o_term > 0:
             terms.append( "(%+ 6.2f%%)" % ( 100 * ( 1 - f_term / o_term ) ) )
@@ -445,7 +329,7 @@ with open( "/dev/null" if options.csv is None else options.csv, 'w' ) as out:
     # cannot write header until we know how many fitness values there are...
     need_header = True
     with open( args[ 0 ] ) as fh:
-        p = parser( fh )
+        p = LogParser( fh, options.stop_after )
         source = statsFilter( p.getEntries() )
         if options.filter == "best":
             source = bestFilter( source )
@@ -466,7 +350,7 @@ with open( "/dev/null" if options.csv is None else options.csv, 'w' ) as out:
                 if need_header:
                     n = len( entry.fitness )
                     header = [ "generation", "eval number" ]
-                    if options.sort is not None:
+                    if options.improvement is not None:
                         n >>= 1
                         header += [ "improvement %d" % i for i in range( n ) ]
                     header += [ "fitness %d" % i for i in range( n ) ]
@@ -516,8 +400,8 @@ if options.csv is None:
         orig_values = [ 1 / x - 1 for x in original.fitness ]
         print "original values:      ", show_fitness( orig_values, orig_values )
     print "Pareto frontier:"
-    for fitness in sorted( getBest() ):
-        best_values = [ 1 / x - 1 for x in fitness ]
+    for e in sorted( bestFilter( [] ), key = lambda e: float( e.fitness[ 0 ] ) ):
+        best_values = [ 1 / x - 1 for x in e.fitness ]
         print "    best values:      ", show_fitness( best_values, orig_values )
     print "variants considered:  ", numEntries
     print "unique variants:      ", len( unique )
--- lib/genprogutil.py
+++ lib/genprogutil.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+from collections import namedtuple
 from contextlib import contextmanager
 import os
 import re
@@ -89,6 +90,159 @@ class GenProgEnv:
                     yielded = True
                 if not yielded:
                     yield [ 0.0 ]
+
+Entry = namedtuple( "Entry", "gen evalno fitness variant" )
+
+class Interval:
+    def __init__( self, mean, delta ):
+        self.mean  = mean
+        self.delta = delta
+
+    def __neg__( self ):
+        return Interval( - self.mean, self.delta )
+
+    def __float__( self ):
+        return self.mean
+
+    def __lt__( self, other ):
+        if isinstance( other, Interval ):
+            return self.mean + self.delta < other.mean - other.delta
+        else:
+            return self.mean + self.delta < other
+
+    def __gt__( self, other ):
+        if isinstance( other, Interval ):
+            return self.mean - self.delta > other.mean + other.delta
+        else:
+            return self.mean - self.delta > other
+
+    def __eq__( self, other ):
+        if not isinstance( other, Interval ):
+            raise NotImplemented
+        return self.mean == other.mean and self.delta == other.delta
+
+    def __add__( self, other ):
+        if isinstance( other, Interval ):
+            return Interval( self.mean + other.mean, self.delta + other.delta )
+        else:
+            return Interval( self.mean + other, self.delta )
+
+    def __sub__( self, other ):
+        if isinstance( other, Interval ):
+            return Interval( self.mean - other.mean, self.delta + other.delta )
+        else:
+            return Interval( self.mean - other, self.delta )
+
+    def __rdiv__( self, other ):
+        if isinstance( other, Interval ):
+            return other.mean / self.mean
+        else:
+            return other / self.mean
+
+    def __str__( self ):
+        return "%s +/- %s" % ( str( self.mean ), str( self.delta ) )
+
+    def __repr__( self ):
+        return "Interval(%s,%s)" % ( repr( self.mean ), repr( self.delta ) )
+
+    def __hash__( self ):
+        return hash( ( self.mean, self.delta ) )
+
+class LogParser:
+    def __init__( self, stream, max_variants = None ):
+        self.CONFIG  = 0x1
+        self.DEBUG   = 0x2
+        self.FITNESS = 0x4
+
+        self.stream = self._getLine( stream )
+        self.max_count = max_variants
+        self.fitness = list()
+        self.config = list()
+        self.debug = list()
+
+        next( self.stream )
+
+    def _getLine( self, stream ):
+        go_until = self.CONFIG | self.DEBUG | self.FITNESS
+
+        # A pattern for floating point numbers. This appears a couple times in
+        # the variant pattern, so I made a separate string to interpolate in
+        # instead of copying it verbatim and making a single extra-long pattern.
+        # Note that the pattern here does not capture any groups: the (?:) glyph
+        # indicates a non-capturing group in python.
+
+        f = r"(?:nan|[-+]?inf|[-+]?\d+(?:\.\d+)?(?:[eE][-+]?\d+)?)"
+        variant_pat = re.compile(
+            r"^\t\s*((?:{0}\s+(?:\+/-\s+{0}\s+)?)+)(.*)".format( f )
+        )
+        generation_pat = re.compile( r"generation (\d+) " )
+
+        gen = 0
+        count = 0
+        for line in stream:
+            m = variant_pat.search( line )
+            if m is not None:
+                if self.max_count is not None and self.max_count <= count:
+                    continue
+                count += 1
+                variant = m.group( 2 )
+                fitnesses, intervals = list(), list()
+                prev = None
+                for term in m.group( 1 ).split():
+                    if len( fitnesses ) == len( intervals ):
+                        fitnesses.append( float( term ) )
+                    elif prev == "+/-":
+                        intervals.append( float( term ) )
+                    elif term != "+/-":
+                        intervals.append( 0 )
+                        fitnesses.append( float( term ) )
+                    prev = term
+                if len( intervals ) < len( fitnesses ):
+                    intervals.append( 0 )
+                fitness = tuple( [
+                    Interval( m, d ) for m, d in zip( fitnesses, intervals )
+                ] )
+                self.fitness.append( Entry( gen, count, fitness, variant ) )
+                if go_until & self.FITNESS != 0:
+                    go_until = yield
+                continue
+            m = generation_pat.search( line )
+            if m is not None:
+                gen = int( m.group( 1 ) )
+                continue
+            if line.startswith( "--" ):
+                self.config.append( line.split( None, 1 ) )
+                if go_until & self.CONFIG != 0:
+                    go_until = yield
+                continue
+            self.debug.append( line )
+            if go_until & self.DEBUG != 0:
+                go_until = yield
+
+    def _consumer( self, queue, key ):
+        flipped = list()
+        while True:
+            while len( flipped ) > 0:
+                yield flipped.pop()
+            flipped = list( reversed( queue ) )
+            del queue[ : ]
+            if len( flipped ) == 0:
+                try:
+                    self.stream.send( key )
+                except StopIteration:
+                    break
+
+    def getConfig( self ):
+        for entry in self._consumer( self.config, self.CONFIG ):
+            yield entry
+
+    def getDebug( self ):
+        for entry in self._consumer( self.debug, self.DEBUG ):
+            yield entry
+
+    def getEntries( self ):
+        for entry in self._consumer( self.fitness, self.FITNESS ):
+            yield entry
 
 def lower_genome( genes ):
     if genes == [ "original" ]:
