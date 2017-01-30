599697effc37e67c0e380797748b57ed1c7fa3d4
Jonathan Dorn <dornja@gmail.com>
2017-01-30 13:36:18 -0500

    added flags to submit.py to control run-experiment.py

--- bin/submit.py
+++ bin/submit.py
@@ -4,6 +4,7 @@ import base64
 from cStringIO import StringIO
 from optparse import OptionParser
 import os
+import pipes
 from subprocess import PIPE, Popen
 
 parser = OptionParser(
@@ -11,6 +12,22 @@ parser = OptionParser(
 )
 parser.add_option(
     "--bmark", metavar = "bmark", help = "benchmark to run"
+)
+parser.add_option(
+    "--inputs", metavar = "args",
+    help = "comma-separated list of inputs to maximize on (default: empty)"
+)
+parser.add_option(
+    "--no-simplify", action = "store_true",
+    help = "do not minimize pareto frontier"
+)
+parser.add_option(
+    "--stop-after", metavar = "N", type = int,
+    help = "stop the search / minimization after N fitness evaluations"
+)
+parser.add_option(
+    "--use-search", metavar = "resultsdir",
+    help = "use the results of a previous search instead of searching again"
 )
 options, args = parser.parse_args()
 
@@ -31,7 +48,16 @@ if options.bmark is None:
 
 script = StringIO()
 print >>script, "#!/bin/sh"
-print >>script, "/localtmp/powergauge/bin/run-experiment.py", options.bmark, results, "<<", "EOF"
+print >>script, "/localtmp/powergauge/bin/run-experiment.py",
+if options.inputs is not None:
+    print >>script, "--inputs", options.inputs,
+if options.no_simplify:
+    print >>script, "--no-simplify",
+if options.stop_after is not None:
+    print >>script, "--stop-after", options.stop_after,
+if options.use_search is not None:
+    print >>script, "--use-search", pipes.quote( options.use_search ),
+print >>script, options.bmark, results, "<<", "EOF"
 with open( config ) as fh:
     base64.encode( fh, script )
 print >>script, "EOF"
@@ -39,7 +65,10 @@ print >>script, "EOF"
 # always submit from the home directory...
 
 os.chdir( os.path.expanduser( "~" ) )
-p = Popen( [ "sbatch", "--exclusive" ] + slurmopts, stdin = PIPE )
+p = Popen(
+    [ "sbatch", "--exclusive", "--job-name", results ] + slurmopts,
+    stdin = PIPE
+)
 p.communicate( script.getvalue() )
 p.wait()
 exit( p.returncode )
