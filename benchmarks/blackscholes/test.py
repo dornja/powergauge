22af3677d766d4232943be9ce398b217050d5f0e
Jeremy Lacomis <lacomis@virginia.edu>
2017-03-30 11:08:20 -0400

    Backup blackscholes inputs

--- benchmarks/blackscholes/test.py
+++ benchmarks/blackscholes/test.py
@@ -2,20 +2,29 @@
 
 import numpy as np
 import os
+from subprocess import check_call
 import sys
 
 root = os.path.abspath( sys.argv[ 0 ] )
 for i in range( 3 ):
     root = os.path.dirname( root )
 sys.path.append( os.path.join( root, "lib" ) )
-from testutil import ParallelTest
+from testutil import Multitmp, ParallelTest
+from util import mktemp
 
 # Run test.py -h to get usage information
 
 class BlackscholesTest( ParallelTest ):
-    def getCommand( self, outfile ):
-        cmd = [ self.exe, "1" ]
-        cmd += {
+    def __init__( self, *args ):
+        self.__backup = None
+        ParallelTest.__init__( self, *args )
+
+    def __del__( self ):
+        if self.__backup is not None:
+            self.__backup[ 0 ].__exit__( None, None, None )
+
+    def _getInputArgs( self ):
+        return {
             "test":   [ "inputs/in_4.txt" ],
             "tiny":   [ "inputs/in_16.txt" ],
             "small":  [ "inputs/in_4K.txt" ],
@@ -23,11 +32,26 @@ class BlackscholesTest( ParallelTest ):
             "large":  [ "inputs/in_64K.txt" ],
             "huge":   [ "inputs/in_10M.txt" ],
         }[ self.size ]
-        cmd += [ outfile ]
+
+
+    def checkArgs( self, *args ):
+        result = ParallelTest.checkArgs( self, *args )
+        context = mktemp()
+        self.__backup = context, context.__enter__()
+        check_call( [
+            "rsync", "-a", self._getInputArgs()[ 0 ], self.__backup[ 1 ]
+        ] )
+        return result
+
+    def getCommand( self, outfile ):
+        cmd = [ self.exe, "1" ] + self._getInputArgs() + [ outfile ]
         return cmd, dict()
 
     def getParallelFitness( self, *args ):
         results = ParallelTest.getParallelFitness( self, *args )
+        check_call( [
+            "rsync", "-a", self.__backup[ 1 ], self._getInputArgs()[ 0 ]
+        ] )
         if self.options.error:
             if results == [ [ 0 ] ]:
                 return [ [ 0 ], [ 0 ] ]
