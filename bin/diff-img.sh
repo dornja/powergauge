87d4a1fffb7613253e12365d04c4d08a7389d34c
Jeremy Lacomis <lacomis@virginia.edu>
2017-04-20 14:50:47 -0400

    Fix bug in diff-img script

--- bin/diff-img.sh
+++ bin/diff-img.sh
@@ -3,7 +3,14 @@
 DIR1="$1"
 DIR2="$2"
 
-for file in `find "$DIR1" -type f`; do
+FILES1=`cd $DIR1; find . -type f | sort`
+FILES2=`cd $DIR2; find . -type f | sort`
+
+if [ "x$FILES1" != "x$FILES2" ]; then
+    exit 1
+fi
+
+for file in $FILES1; do
     fname=`basename "$file"`
     if diff "$DIR1/$fname" "$DIR2/$fname"; then
 	echo "total error: 0"
